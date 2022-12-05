8bac27520eb81264b05f4d7a275b2f5a302b9d4b
tenheadedlion <16246256+tenheadedlion@users.noreply.github.com>
2022-12-06 07:20:01 +0800

    add dynamic provider use case

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
+import { LoggerModule } from './logger/logger.module';
 
 @Module({
-  imports: [CatsModule],
+  imports: [CatsModule, LoggerModule],
   controllers: [AppController],
   providers: [
     AppService,
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { LoggerService } from './logger/logger.service';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly logger: LoggerService) {
+    this.logger.setPrefix('AppService');
+  }
   getHello(): string {
+    this.logger.log('Hello World');
     return 'Hello World!';
   }
 }
